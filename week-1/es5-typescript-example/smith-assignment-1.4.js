491103f22bcfa0bb4781038111afe60fd0e129eb
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2018-12-17 12:15:23 -0600

    Added Assignments 1.4 and 1.5

---
+++ week-1/es5-typescript-example/smith-assignment-1.4.js
@@ -0,0 +1,6 @@
+var favoriteFood = "lutefisk";
+var vomitCount = 285462;
+function tryToEatSomeLutefisk(favoriteFood, vomitCount) {
+    return "Dear Diary,\n Today I ate some " + favoriteFood + " and I only vomited " + vomitCount + " times!";
+}
+console.log(tryToEatSomeLutefisk(favoriteFood, vomitCount));
---
+++ week-1/es5-typescript-example/smith-assignment-1.4.ts
@@ -0,0 +1,8 @@
+let favoriteFood: string = "lutefisk";
+let vomitCount: number = 285462;
+
+function tryToEatSomeLutefisk(favoriteFood: string, vomitCount: number) {
+    return `Dear Diary,\n Today I ate some ${favoriteFood} and I only vomited ${vomitCount} times!`;
+}
+
+console.log(tryToEatSomeLutefisk(favoriteFood, vomitCount));

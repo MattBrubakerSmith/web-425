86cd71c32ac2c1248934387ebfdf2acf23d86c19
Matthew Smith <mrsmaft@gmail.com>
2019-01-28 13:35:53 -0600

    Adding Exercise 5.2 and initialized Exercise 5.3 and Assignment 5.4

---
+++ week-5/smith-md-toolbar/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
